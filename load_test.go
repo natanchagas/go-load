06b8124a8c9744584e6ee381fd099ec768cd0a6e
natanchagas <natan.c.silva@uol.com.br>
2022-02-28 01:32:19 -0300

    test: :white_check_mark: add test
    add test to constructor

---
+++ load_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestNewLoadConfig(t *testing.T) {
+	assert := func(t testing.TB, got LoadConfig, want LoadConfig) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got: %v,\nWant: %v", got, want)
+		}
+	}
+
+	t.Run("Ramp up load test", func(t *testing.T) {
+		loadConfig := NewLoadConfig(10, 10, up, 2)
+
+		desired := LoadConfig{
+			Users:    10,
+			Duration: time.Duration(10 * time.Second),
+			Ramp: Ramp{
+				RampMode:   up,
+				RampAmount: 2,
+			},
+		}
+
+		assert(t, loadConfig, desired)
+	})
+
+	t.Run("Ramp down load test", func(t *testing.T) {
+		loadConfig := NewLoadConfig(20, 20, down, 4)
+
+		desired := LoadConfig{
+			Users:    20,
+			Duration: time.Duration(20 * time.Second),
+			Ramp: Ramp{
+				RampMode:   down,
+				RampAmount: 4,
+			},
+		}
+
+		assert(t, loadConfig, desired)
+	})
+
+}
